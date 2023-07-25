226a054e2829aaaeb9cea318428be375b6a54759
Anne-15 <wariiyuanne@gmail.com>
2023-07-25 05:13:20 -0700

    printing value

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,13 +1,13 @@
 import sys
 
 def print_arguments():
-    num_arguments = len(sys.argv) - 1
+    num_arguments = len(sys.argv)-1
     if num_arguments == 0:
         print("Number of arguments: .")
         return
-    print("Number of arguments: .", num_arguments)
+    print("Number of arguments: .",num_arguments)
     print("Arguments:")
-    for j, arg in enumerate(sys.argv[1:], start=1):
-        print("{}: {}".format(j, argv))
+    for j, arg in enumerate(sys.argv[1:],start=1):
+        print("{}:{}".format(j,argv))
 if __name__ == '__main__':
     print_arguments()
