dc8f708995f0204cd594fcbff1a6ba4b437d6047
Anne-15 <wariiyuanne@gmail.com>
2023-07-25 04:21:02 -0700

    using immport function from another file

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,4 +1,4 @@
-import add from python-functions
+import 0-sum from python-functions
 a = 1
 b = 2
-print ("{} + {} = {}".format(a, b, add(a,b)))
+print ("{} + {} = {}".format(a, b, 0-sum.add(a,b)))
