078780f0a0881455b66ecf958ae0f3b5759e0a55
Anne-15 <wariiyuanne@gmail.com>
2023-07-26 10:22:59 -0700

    making changes

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,11 +1,13 @@
-def print_arguments(*args):
-    def check(a):
-        print(len(args))
-        if(len(args) == 0):
-            print(".")
-        else:
-            index = 1
-            for n in args:
-                print(index, ":", n)
-                index += 1
-    return check(args)
+if __name__ == '__main__':
+
+    def print_arguments(*args):
+        def check(a):
+            print(len(args))
+            if(len(args) == 0):
+                print(".")
+            else:
+                index = 1
+                for n in args:
+                    print(index, ":", n)
+                    index += 1
+        return check(args)
