be28b43aae2745a7fcb4e5ecc885ea1161735cce
Anne-15 <wariiyuanne@gmail.com>
2023-07-31 12:07:36 +0300

    getting square by number

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -14,3 +14,30 @@ class Square:
             size (int): The size of the square.
         """
         self.__size = size
+        
+    def area(self):
+        """
+        Calculates and returns the area of the square.
+
+        Returns:
+            int: The area of the square.
+        """
+        return self.__size ** 2
+
+    def perimeter(self):
+        """
+        Calculates and returns the perimeter of the square.
+
+        Returns:
+            int: The perimeter of the square.
+        """
+        return 4 * self.__size
+
+    def __str__(self):
+        """
+        Returns a string representation of the Square object.
+
+        Returns:
+            str: A string representation of the Square object.
+        """
+        return f"Square with size: {self.__size}"
