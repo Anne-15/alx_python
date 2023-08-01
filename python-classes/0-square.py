def9b8b1b716bf6c6eb757b4ed75b7931a6a0fe1
Anne-15 <wariiyuanne@gmail.com>
2023-08-01 12:59:39 +0300

    making changes

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -7,37 +7,5 @@ class Square:
     """
 
     def __init__(self, size):
-        """
-        Initializes a Square instance with a given size.
-
-        Args:
-            size (int): The size of the square.
-        """
         self.__size = size
         
-    def area(self):
-        """
-        Calculates and returns the area of the square.
-
-        Returns:
-            int: The area of the square.
-        """
-        return self.__size ** 2
-
-    def perimeter(self):
-        """
-        Calculates and returns the perimeter of the square.
-
-        Returns:
-            int: The perimeter of the square.
-        """
-        return 4 * self.__size
-
-    def __str__(self):
-        """
-        Returns a string representation of the Square object.
-
-        Returns:
-            str: A string representation of the Square object.
-        """
-        return f"Square with size: {self.__size}"
