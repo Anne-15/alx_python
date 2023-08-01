0d0508b7dc11da61829552d443bb32e4d62c2ded
Anne-15 <wariiyuanne@gmail.com>
2023-08-01 13:00:32 +0300

    making changes

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,11 +1,10 @@
+"""
+Represents a square with a given size.
+
+Attributes:
+    __size (int): The size of the square (private).
+"""
 class Square:
-    """
-    Represents a square with a given size.
-
-    Attributes:
-        __size (int): The size of the square (private).
-    """
-
     def __init__(self, size):
         self.__size = size
         
