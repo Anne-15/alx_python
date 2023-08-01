1eee30667d162634f52b6c347574f7f5c1fbd693
Anne-15 <wariiyuanne@gmail.com>
2023-08-01 13:29:24 +0300

    making changes

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -12,15 +12,17 @@ class Square:
         __size (int): The size of the square (private).
     """
     def __init__(self, size=0):
+        self.__size = size
+    def size(self, size):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
-    def area(self):
-        return self.__size ** 2
     def size(self):
         return self.__size
-    def size(self, size):
-        self.__size = size
+    def area(self):
+        return self.__size ** 2
+    
+    
