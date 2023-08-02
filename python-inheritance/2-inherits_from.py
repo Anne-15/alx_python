c9a4e2e5c6532b8c6a50aad7c656d7592bea1b05
Anne-15 <wariiyuanne@gmail.com>
2023-08-02 13:19:09 +0300

    adding spaces

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -2,11 +2,14 @@
 Represents a function that checks subclass.
 
 """
+
 def inherits_from(obj, a_class):
+
     """
     Represents a function that checks subclass.
 
     """
+
     if isinstance(obj, a_class):
         return True
     else:
