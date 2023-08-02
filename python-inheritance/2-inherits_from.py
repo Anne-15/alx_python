260ae483647155a8ada6f40e6d8f5b45028f1443
Anne-15 <wariiyuanne@gmail.com>
2023-08-02 11:23:56 +0300

    only subclass of

---
+++ python-inheritance/2-inherits_from.py
@@ -0,0 +1,10 @@
+"""
+Represents a function that checks subclass.
+
+"""
+def inherits_from(obj, a_class):
+    """
+    Represents a function that checks subclass.
+
+    """
+    return isinstance(obj, a_class)
