1ae6f59e7b7c0b02f6d1a7597347a643cfdc7193
Anne-15 <wariiyuanne@gmail.com>
2023-08-02 13:17:06 +0300

    making changes

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -7,4 +7,7 @@ def inherits_from(obj, a_class):
     Represents a function that checks subclass.
 
     """
-    return isinstance(obj, a_class)
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
