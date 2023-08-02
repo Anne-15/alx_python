0114afb49f74c4135fa3ad90bc6dfe75592dddf5
Anne-15 <wariiyuanne@gmail.com>
2023-08-02 10:48:10 +0300

    adding documentation

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -1,4 +1,12 @@
+"""
+Represents a function that checks subclass.
+
+"""
 def is_same_class(obj, a_class):
+    """
+    Represents a function that checks subclass.
+
+    """
     if issubclass(obj, a_class):
         return True
     else:
