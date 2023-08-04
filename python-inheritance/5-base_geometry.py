f44e616879036d9db96b803f0aa76b1dad39f030
Anne-15 <wariiyuanne@gmail.com>
2023-08-04 17:04:13 +0300

    making changes

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -29,6 +29,7 @@ class BaseGeometry:
             __size (int): The size of the square (private).
         """
         raise Exception("area() is not implemented")
+    
     def integer_validator(self, name, value):
         """
         Represents a square with a given size.
@@ -37,6 +38,6 @@ class BaseGeometry:
             __size (int): The size of the square (private).
         """
         if not isinstance(value, int):
-            raise TypeError("{} must be an integer".format(self.name))
+            raise TypeError("{} must be an integer".format(name))
         elif value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
