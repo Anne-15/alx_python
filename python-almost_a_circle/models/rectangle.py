80b20ef1f11cd97e6b3acdeebe6a33248c2a03b4
Anne-15 <wariiyuanne@gmail.com>
2023-08-07 10:45:06 +0300

    args

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -131,3 +131,11 @@ class Rectangle(Base):
 
     def __str__(self):
         return ("[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x, self.__y, self.__width, self.__height))
+    
+    def update(self, *args):
+        """
+        Represents a function that updates the arguments
+
+        """
+        for _ in args:
+            print ("[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x, self.__y, self.__width, self.__height))
