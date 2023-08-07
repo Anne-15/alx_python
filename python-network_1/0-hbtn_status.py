2e38dabbd5e9959e4bdb7e1cf2693817a1e21e36
Anne-15 <wariiyuanne@gmail.com>
2023-08-07 14:29:49 +0300

    making changes

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -3,4 +3,6 @@ Represents how to fetch a url using a link
 
 """
 import requests
-r = requests.get("https://alu-intranet.hbtn.io/status")
+url = "https://alu-intranet.hbtn.io/status"
+header = "-"
+r = requests.fetch(header, url)
