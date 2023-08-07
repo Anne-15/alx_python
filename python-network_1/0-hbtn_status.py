935157c1a01543e7c5008621fe757d5e4107149b
Anne-15 <wariiyuanne@gmail.com>
2023-08-07 14:38:09 +0300

    changes

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -3,11 +3,7 @@ Represents how to fetch a url using a link
 
 """
 import requests
-
-if __name__ == "__main__":
-
-    # url = "https://alu-intranet.hbtn.io/status"
-    r = requests.get("https://alu-intranet.hbtn.io/status")
-    print("Body response:")
-    print("\t- type:", type(r.text))
-    print("\t- content:", r.text)
+r = requests.get("https://alu-intranet.hbtn.io/status")
+print("Body response:")
+print("\t- type:", type(r.text))
+print("\t- content:", r.text)
