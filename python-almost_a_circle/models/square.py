de12b7d40472e258ab873efc96a3a4468be61637
Anne-15 <wariiyuanne@gmail.com>
2023-08-07 11:49:20 +0300

    getters and setters

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -176,3 +176,23 @@ class Square(Rectangle):
 
     def __str__(self):
         return ("[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.size))
+    
+    @property
+    def size(self):
+        return self.width 
+    
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value <= 0:
+            raise ValueError("width must be > 0")
+        else:
+            self.width = value
+        
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value <= 0:
+            raise ValueError("width must be > 0")
+        else:
+            self.height = value
