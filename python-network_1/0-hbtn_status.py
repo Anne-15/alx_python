227499c6ee5a8cde3b1546516049ee874d1b8d5e
Anne-15 <wariiyuanne@gmail.com>
2023-08-07 14:37:21 +0300

    changes

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -6,8 +6,8 @@ import requests
 
 if __name__ == "__main__":
 
-    url = "https://alu-intranet.hbtn.io/status"
-    r = requests.get(url)
+    # url = "https://alu-intranet.hbtn.io/status"
+    r = requests.get("https://alu-intranet.hbtn.io/status")
     print("Body response:")
     print("\t- type:", type(r.text))
     print("\t- content:", r.text)
