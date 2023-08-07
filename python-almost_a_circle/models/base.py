db465518977064ed8efcb6d90875a3968200762e
Anne-15 <wariiyuanne@gmail.com>
2023-08-07 09:34:55 +0300

    creating base class

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,17 @@
+"""
+Represents a base class
+
+"""
+class Base:
+    __nb_object = 0
+    def __init__(self, id=None):
+        """
+        Represents a base class function
+
+        """
+        self.id = id
+        if id != None:
+            self.id = id
+        else:
+            Base.__nb_object += 1
+            self.id = Base.__nb_object
