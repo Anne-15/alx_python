5ccae717013c7861a4599387f1288a171921cdfd
Anne-15 <wariiyuanne@gmail.com>
2023-08-07 09:10:55 +0300

    making changes

--- python-inheritance/8-square.py
+++ python-inheritance/8-square.py
@@ -4,10 +4,34 @@ Represents a square with a given size.
 Attributes:
     __size (int): The size of the square (private).
 """
-class BaseGeometry():
+class BaseGeometryMeta(type):
     """
     Represents a class
     """
+    def __dir__(self):
+        """
+        Get a list of attributes for this class and exclude
+        the __init_superclass
+
+        """
+        attributes = super().__dir__()
+        new_attributes = [item for item in attributes if item != "__init_subclass__"]
+        return new_attributes
+
+class BaseGeometry(metaclass=BaseGeometryMeta):
+    """
+    Represents a class
+    """
+    def __dir__(self):
+        """
+        Get a list of attributes for this class and exclude
+        the __init_superclass
+
+        """
+        attributes = super().__dir__()
+        new_attributes = [item for item in attributes if item != "__init_subclass__"]
+        return new_attributes
+    
     def area(self):
         """
         Represents a square with a given size.
