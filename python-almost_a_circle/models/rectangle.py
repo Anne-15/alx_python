aa45aa56af8f2841bcb3b58254176a8919dc4b8b
Anne-15 <wariiyuanne@gmail.com>
2023-08-08 19:50:31 +0300

    importing the classes

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,25 +1,13 @@
 """
+Represents an import class
+
+"""
+from base import Base
+
+"""
 Represents a base class
 
 """
-class Base:
-    """
-    Represents a base class
-
-    """
-    __nb_object = 0
-    def __init__(self, id=None):
-        """
-        Represents a base class function
-
-        """
-        self.id = id
-        if id != None:
-            self.id = id
-        else:
-            Base.__nb_object += 1
-            self.id = Base.__nb_object
-
 class Rectangle(Base):
     """
     Represents a class that inherits from base
