1ad213dbcbc563926aa7af35b9024e0055cb44e7
Anne-15 <wariiyuanne@gmail.com>
2023-08-09 15:29:07 +0300

    adding email

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -5,4 +5,8 @@ Represents how to fetch a url using a link
 import requests
 import sys
 
-r = requests.post()
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+    request = requests.post(url, params=email)
+    print(request)
