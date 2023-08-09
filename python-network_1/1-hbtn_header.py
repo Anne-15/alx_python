732c1e7fa0c96326321458df8f315bb1c581ec64
Anne-15 <wariiyuanne@gmail.com>
2023-08-09 23:54:06 +0300

    changes

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -7,6 +7,7 @@ import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    r = requests.get(url)
+    r = requests.get(url, allow_redirects=True)
+    print(r.headers)
     x_request_id = r.headers.get('X-Request-Id')
     print(x_request_id)
