b9a4cae65c3e759107fd7c70aa24403e1313d959
Anne-15 <wariiyuanne@gmail.com>
2023-08-09 13:55:19 +0300

    changes

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,12 +1,19 @@
-"""
-Represents how to fetch a url using a link
-
-"""
 import requests
 import sys
 
 if __name__ == "__main__":
-    url = sys.argv[0]
-    r = requests.get(url)
-    x_request_id = r.headers.get('X-Request-Id')
-    print(x_request_id)
+    if len(sys.argv) != 2:
+        print("Usage: python your_script.py <URL>")
+        sys.exit(1)
+
+    url = sys.argv[1]
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        x_request_id = response.headers.get('X-Request-Id')
+        if x_request_id:
+            print(x_request_id)
+        else:
+            print("No X-Request-Id header found in the response.")
+    else:
+        print(f"Request failed with status code: {response.status_code}")
