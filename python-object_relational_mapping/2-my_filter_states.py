5eceb734fb94f5f36eae7d09b49682844210ca7b
Anne-15 <wariiyuanne@gmail.com>
2023-08-22 16:08:51 +0300

    filter by name

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -12,14 +12,14 @@ state_name_searched = sys.argv[4]
 
 db = MySQLdb.connect(
     host='localhost',
-    user=username, 
-    password=password, 
-    database=database, 
+    user=username,
+    password=password,
+    database=database,
     port=3306
-    ),
+    )
 
-cur = db.connect(),
-cur.que(""" SELECT * FROM states WHERE name IS EQUAL TO ${state_name_searched} ORDER BY id""") 
+cur = db.cursor(),
+cur.execute(""" SELECT * FROM states WHERE name = '${}' ORDER BY id""".format(state_name_searched)) 
 states = cur.fetchall()
 cur.close()
 db.close()
