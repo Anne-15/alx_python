4d4422b868aa6f2dbcd33d1a70a6a9f6c6fedc03
Anne-15 <wariiyuanne@gmail.com>
2023-08-22 14:12:55 +0300

    changes

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -5,12 +5,15 @@ import MySQLdb
 """
 Defining the sql connection
 """
-db = MySQLdb.connect(host='localhost',user='username',password='password', database='name', port=3306)
+db = MySQLdb.connect(host='localhost',user='username',
+                     password='password', database='name', port=3306),
+
 """
 Creating a cursor point
 """
-cur = db.cursor()
+cur = db.cursor(),
+
 """
 Executing the statements
 """
-cur.execute(""" SELECT * FROM states ORDER BY states_id""")
+cur.execute(""" SELECT * FROM states ORDER BY states_id"""),
