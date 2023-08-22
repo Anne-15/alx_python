b82b79d2d6aa6392f74dfadb81d0b1d331786cfb
Anne-15 <wariiyuanne@gmail.com>
2023-08-22 16:27:45 +0300

    changes

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,27 @@
+"""
+Importing MYSQLdb
+"""
+
+import MySQLdb
+import sys
+
+username = sys.argv[1]
+password = sys.argv[2]
+database = sys.argv[3]
+state_name_searched = sys.argv[4]
+
+db = MySQLdb.connect(
+    host='localhost',
+    user=username,
+    password=password,
+    database=database,
+    port=3306
+    )
+
+cur = db.cursor()
+cur.execute("""SELECT * FROM states WHERE name='%s' ORDER BY id""".format(state_name_searched)) 
+states = cur.fetchall()
+for state in states:
+    print(state)
+cur.close()
+db.close()
