60e869dbe32d147ac4affb73e56ad3a41b51a401
Anne-15 <wariiyuanne@gmail.com>
2023-08-22 15:50:52 +0300

    changes

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -22,7 +22,5 @@ if __name__ == "__main__":
     cur = db.cursor()
     cur.execute(""" SELECT * FROM states ORDER BY id""") 
     states = cur.fetchall()
-    for state in states:
-        print(states)
     cur.close()
     db.close()
