7ff8b6b89899114f97277d82111ac0949b4ce81b
Anne-15 <wariiyuanne@gmail.com>
2023-08-22 20:53:56 +0300

    all states via sqlalchemy

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,9 +1,23 @@
+#!/usr/bin/python3
+"""
+Module containing the State class definition and Base instance
+"""
+
+from sqlalchemy import create_engine, select
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
-import sqlalchemy
 import sys
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
-    
+
+    engine = create_engine("mysql+mysqldb://username\
+                           :password@localhost:3306/dbname")
+    Session = sessionmaker(bind=engine)
+    session = Session(engine)
+    stmt = select(State).order_by(State.id)
+    for user in session.scalars(stmt):
+        print(user)
