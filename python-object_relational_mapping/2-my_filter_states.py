2272a1d948806bbb192297307336fb87cf61d702
Anne-15 <wariiyuanne@gmail.com>
2023-08-22 16:13:35 +0300

    using format

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -19,8 +19,7 @@ db = MySQLdb.connect(
     )
 
 cur = db.cursor(),
-query = "SELECT * FROM states WHERE name='%s ORDER BY id"
-cur.execute(query, state_name_searched) 
+cur.execute("""SELECT * FROM states WHERE name='%s ORDER BY id""".format(state_name_searched)) 
 states = cur.fetchall()
 for state in states:
     print(state)
