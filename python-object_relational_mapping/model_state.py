627ceb0c91c8536d554a3e2f74aa5e10ba457a94
Anne-15 <wariiyuanne@gmail.com>
2023-08-22 17:53:43 +0300

    docs

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,18 +1,23 @@
 """
-Import statements
-"""
+Represents the use of sqlalchemy orm.
 
+"""
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
 Base = declarative_base()
 
 """
-Creating a class
-"""
+Represents a square with a given size.
 
+Attributes:
+    __size (int): The size of the square (private).
+"""
 class States(Base):
     """
-    Creating table of the class
+    Represents a square with a given size.
+
+    Attributes:
+        __size (int): The size of the square (private).
     """
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True)
