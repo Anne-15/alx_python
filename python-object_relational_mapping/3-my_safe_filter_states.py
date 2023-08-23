d3d7c71fc2376db041389f54c1a05017734c341e
Anne-15 <wariiyuanne@gmail.com>
2023-08-23 16:05:43 +0300

    changes

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -20,7 +20,7 @@ db = MySQLdb.connect(
 
 cur = db.cursor()
 query = "SELECT * FROM states WHERE name='{}' ORDER BY id"
-cur.execute(query, (state_name_searched))
+cur.execute(query.format(state_name_searched))
 states = cur.fetchall()
 for state in states:
     print(state)
