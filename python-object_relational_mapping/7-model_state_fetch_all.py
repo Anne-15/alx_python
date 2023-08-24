ffb7031a33009e82f5fb214dccdfa1b224920ff7
Anne-15 <wariiyuanne@gmail.com>
2023-08-24 23:23:59 +0300

    changess?

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -3,7 +3,7 @@
 Module containing the State class definition and Base instance
 """
 
-from sqlalchemy import create_engine, select
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 import sys
@@ -13,7 +13,9 @@ if __name__ == "__main__":
     password = sys.argv[2]
     database = sys.argv[3]
 
-    engine = create_engine("mysql+mysqldb://%s:%s@localhost:3306/%s", (username, password, database,))
+    engine = create_engine("mysql+mysqldb://%s:\
+                           %s@localhost:3306/%s",
+                           (username, password, database,))
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
