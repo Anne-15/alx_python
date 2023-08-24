538c72ca88a90cbb3e4b818c05ff013e9d8ad0b0
Anne-15 <wariiyuanne@gmail.com>
2023-08-24 23:26:08 +0300

    changes

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -15,7 +15,8 @@ if __name__ == "__main__":
 
     engine = create_engine("mysql+mysqldb://%s:\
                            %s@localhost:3306/%s",
-                           (username, password, database,))
+                           (username, password, database,),
+                           pool_pre_ping=True)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
