520ca86d5e1e438825d0f862bbd379ff75989d21
Anne-15 <wariiyuanne@gmail.com>
2023-08-24 23:47:39 +0300

    changes

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -3,7 +3,7 @@
 Module containing the State class definition and Base instance
 """
 
-from sqlalchemy import create_engine, select
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 import sys
@@ -19,8 +19,10 @@ if __name__ == "__main__":
     # Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    states = session.query(State).order_by(State.id)
-    for state in states:
-        print("{}: {}".format(state.id, state.name))
-
-    session.close()
+    new_states = ["California", "Arizona", "Texas", "New York", "Nevada"]
+    for state_name in new_states:
+        state = session.query(State).filter_by(name=state_name).first()
+        if not state:
+            new_state = State(name=state_name)
+            session.add(new_state)
+            session.commit()
