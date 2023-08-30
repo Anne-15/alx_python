73add87d50ef5f5405b293aaf961ba3cb177fa70
Anne-15 <wariiyuanne@gmail.com>
2023-08-30 20:24:53 +0300

    adding the url

--- python-web_framework/0-hello_route.py
+++ python-web_framework/0-hello_route.py
@@ -9,6 +9,9 @@ calling the routes
 """
 
 app = Flask(__name__)
-@app.route("/")
+@app.route("/", strict_slashes=False)
 def hello_world():
     return "<p>Hello HBNB!</p>"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
