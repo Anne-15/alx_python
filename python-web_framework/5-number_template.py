7ed8d5fdd1fd005e2e9d2f658ccc1a2c43620d55
Anne-15 <wariiyuanne@gmail.com>
2023-08-30 21:17:02 +0300

    docs

--- python-web_framework/5-number_template.py
+++ python-web_framework/5-number_template.py
@@ -24,13 +24,13 @@ def c_text(text):
 def python_text(text):
     return "Python {}".format(text.replace("_", " "))
 
-@app.route("/number/<int:n>", strict_slashes=False)
+@app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
-    return f"{n} is a number"
+    return "{} is a number".format(n)
 
-@app.route("/number_template/<int:n>", strict_slashes=False)
-def number(n):
-    return render_template("5-number.html", n=n)
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    return render_template('5-number.html', n=n)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
