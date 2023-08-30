81f3d7bf37ab50126c84d4e3a22c1d03140e6e32
Anne-15 <wariiyuanne@gmail.com>
2023-08-30 21:01:52 +0300

    docs

--- python-web_framework/3-python_route.py
+++ python-web_framework/3-python_route.py
@@ -12,18 +12,30 @@ app = Flask(__name__)
 def hello_hbnb():
     return "Hello HBNB!"
 
+"""
+dynamic routing with default values
+"""
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
     return "HBNB"
 
+"""
+dynamic routing with default values
+"""
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
     return "C {}".format(text.replace("_", " "))
 
+"""
+dynamic routing with default values
+"""
 @app.route('/python/', defaults={"text" | "is cool"}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python_text(text):
     return "Python {}".format(text.replace("_", " "))
 
+"""
+starting the server
+"""
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
