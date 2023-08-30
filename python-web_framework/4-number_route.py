e5710206f3bf28f659c75df0180d74513994a068
Anne-15 <wariiyuanne@gmail.com>
2023-08-30 21:07:34 +0300

    is it a numberr

--- python-web_framework/4-number_route.py
+++ python-web_framework/4-number_route.py
@@ -1,18 +1,17 @@
+#!/usr/bin/python3
 """
-importing flask and initiating it
+Script that starts a Flask web application
 """
 
 from flask import Flask
-"""
-calling the routes
-"""
 
 app = Flask(__name__)
-@app.route("/", strict_slashes=False)
+
+@app.route('/', strict_slashes=False)
 def hello_hbnb():
     return "Hello HBNB!"
 
-@app.route("/hbnb", strict_slashes=False)
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
     return "HBNB"
 
@@ -20,10 +19,14 @@ def hbnb():
 def c_text(text):
     return "C {}".format(text.replace("_", " "))
 
-@app.route('/python/', defaults={"text" | "is cool"}, strict_slashes=False)
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python_text(text):
     return "Python {}".format(text.replace("_", " "))
 
-if __name__ == '__main__':
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+    return f"{n} is a number"
+
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
