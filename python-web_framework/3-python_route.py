36a9f203cdcbe28903c7e3076f58bfe3982f9f3e
Anne-15 <wariiyuanne@gmail.com>
2023-08-30 21:03:32 +0300

    major changes

--- python-web_framework/3-python_route.py
+++ python-web_framework/3-python_route.py
@@ -1,41 +1,28 @@
 #!/usr/bin/python3
 """
-importing flask and initiating it
+Script that starts a Flask web application
 """
+
 from flask import Flask
 
-"""
-calling the routes
-"""
 app = Flask(__name__)
-@app.route("/", strict_slashes=False)
+
+@app.route('/', strict_slashes=False)
 def hello_hbnb():
     return "Hello HBNB!"
 
-"""
-dynamic routing with default values
-"""
-@app.route("/hbnb", strict_slashes=False)
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
     return "HBNB"
 
-"""
-dynamic routing with default values
-"""
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
     return "C {}".format(text.replace("_", " "))
 
-"""
-dynamic routing with default values
-"""
-@app.route('/python/', defaults={"text" | "is cool"}, strict_slashes=False)
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python_text(text):
     return "Python {}".format(text.replace("_", " "))
 
-"""
-starting the server
-"""
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
