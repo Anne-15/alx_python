15344039768517ff4f9cd42d45275ac9734c5f99
Anne-15 <wariiyuanne@gmail.com>
2023-10-02 15:44:21 +0300

    making changes

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,28 +1,7 @@
 import requests
 import sys
 
-# def employee_info(id):
-#     # get user data
-#     user = f'https://jsonplaceholder.typicode.com/users/{id}'
-#     user_data = requests.get(user)
-#     users = user_data.json()
-
-#     # get todos
-#     todo = f'https://jsonplaceholder.typicode.com/users/{id}/todos'
-#     todo_list = requests.get(todo)
-#     todos = todo_list.json()
-
-#     # calculate todos
-#     total_tasks = len(todos)
-#     completed_tasks = sum(1 for task in todos if task['completed'])
-
-#     # display response
-#     print(f'Employee {users["name"]} is done with tasks({completed_tasks}/{total_tasks})')
-
-
-if __name__ == '__main__':
-    id = sys.argv[1]
-    # employee_info(id)
+def employee_info(id):
     # get user data
     user = f'https://jsonplaceholder.typicode.com/users/{id}'
     user_data = requests.get(user)
@@ -44,4 +23,9 @@ if __name__ == '__main__':
     for item in todos:
         if item['completed']:
             print(f"\t{item['title']}")
+
+
+if __name__ == '__main__':
+    id = sys.argv[1]
+    employee_info(id)
 
