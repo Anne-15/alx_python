9f3464a7412e2205bd5a1cc3315d30deec88cae1
Anne-15 <wariiyuanne@gmail.com>
2023-10-02 17:22:49 +0300

    changes

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -5,28 +5,21 @@ Function to get employee information using the id
 
 import requests
 import sys
-
 if __name__ == '__main__':
     id = sys.argv[1]
-    # get user data
     user = f'https://jsonplaceholder.typicode.com/users/{id}'
     user_data = requests.get(user)
     users = user_data.json()
 
-    # get todos
     todo = f'https://jsonplaceholder.typicode.com/users/{id}/todos'
     todo_list = requests.get(todo)
     todos = todo_list.json()
 
-    # calculate todos
     total_tasks = len(todos)
     completed_tasks = sum(1 for task in todos if task['completed'])
 
-    # display response
     print(f'Employee {users["name"]} is done with tasks({completed_tasks}/{total_tasks})')
 
-    # display title of the tasks completed
     for item in todos:
         if item['completed']:
             print(f"\t{item['title']}")
-
