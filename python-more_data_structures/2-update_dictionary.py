214967493ec81e8ff8450378caf5a512b7325e02
Anne-15 <wariiyuanne@gmail.com>
2023-10-03 11:34:55 +0300

    changes?

--- python-more_data_structures/2-update_dictionary.py
+++ python-more_data_structures/2-update_dictionary.py
@@ -1,2 +1,9 @@
 def update_dictionary(a_dictionary, key, value):
     a_dictionary[key] = value
+
+# Example Usage:
+my_dict = {'a': 1, 'b': 2, 'c': 3}
+update_dictionary(my_dict, 'b', 10)  # Replace 'b' with 10
+update_dictionary(my_dict, 'd', 4)   # Add 'd' with value 4
+
+print(my_dict)
