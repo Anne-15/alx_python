de2626278cb3f876fd766d4091a1baed3cfc5fd3
Anne-15 <wariiyuanne@gmail.com>
2023-10-03 13:05:47 +0300

    changes

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -3,35 +3,19 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    # Check if the number of arguments is correct
-    if len(sys.argv) != 4:
-        print("Usage: ./script.py <username> <password> <database>")
-        sys.exit(1)
-
-    # Read MySQL credentials from command line arguments
     username, password, database = sys.argv[1:4]
-
-    # Connect to the MySQL server
-    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
-
-    # Create a cursor object to execute queries
+    db = MySQLdb.connect(host="localhost", port=3306, 
+                         user=username, passwd=password, db=database)
     cursor = db.cursor()
-
     try:
-        # Execute the SQL query
-        cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id;")
-
-        # Fetch all the rows
+        cursor.execute("""SELECT * FROM states WHERE name \
+                       LIKE 'N%' ORDER BY id;""")
         rows = cursor.fetchall()
-
-        # Display the results
         for row in rows:
             print(row)
-
     except Exception as e:
         print(f"Error: {e}")
 
     finally:
-        # Close the cursor and database connection
         cursor.close()
         db.close()
