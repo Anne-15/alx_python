42529280f12e4130dd0e3a7412dc76ec4c5eedbb
Anne-15 <wariiyuanne@gmail.com>
2023-10-03 11:29:58 +0300

    changes

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -3,7 +3,10 @@ import sys
 if __name__=="__main__":
 
     num_arguments = len(sys.argv) - 1
-    print(f"{num_arguments} argument{'s' if num_arguments != 1 else ''}", end='')
+    if(num_arguments == 0):
+        print(f"{num_arguments} argument{'s' if num_arguments != 1 else ''}", end='.')
+    else:
+        print(f"{num_arguments} argument{'s' if num_arguments != 1 else ''}", end='')
 
     if num_arguments > 0:
         print(":", end='')
