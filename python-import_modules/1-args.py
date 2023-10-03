c4ff8ea1c8fdd1b158cc5e393f4c19e47b0f007e
Anne-15 <wariiyuanne@gmail.com>
2023-10-03 11:26:49 +0300

    changes

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,12 +1,14 @@
 import sys
 
-num_arguments = len(sys.argv) - 1
-print(f"{num_arguments} argument{'s' if num_arguments != 1 else ''}", end='')
+if __name__=="__main__":
 
-if num_arguments > 0:
-    print(":", end='')
-    for i, arg in enumerate(sys.argv[1:], start=1):
-        print(f"\n{i}: {arg}", end='')
+    num_arguments = len(sys.argv) - 1
+    print(f"{num_arguments} argument{'s' if num_arguments != 1 else ''}", end='')
 
-# Add a new line
-print()
+    if num_arguments > 0:
+        print(":", end='')
+        for i, arg in enumerate(sys.argv[1:], start=1):
+            print(f"\n{i}: {arg}", end='')
+
+    # Add a new line
+    print()
