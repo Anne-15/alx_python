b5acb7ba33a91be19702dc4602419513df113dc8
Anne-15 <wariiyuanne@gmail.com>
2023-10-03 12:53:36 +0300

    github?

--- python-network_1/6-my_github.py
+++ python-network_1/6-my_github.py
@@ -1,14 +1,26 @@
-"""
-Represents how to fetch a url using a link
-
-"""
+#!/usr/bin/python3
 import requests
 import sys
 
 if __name__ == "__main__":
     username = sys.argv[1]
-    password = sys.argv[2]
-    url = "https://github.com/{Anne-15}"
-    "https://api.github.com/authorizations"
-    r = requests.get(url).json()
-    print(r)
+    access_token = sys.argv[2]
+    
+    try:
+        # Create a basic authentication string
+        auth = (username, access_token)
+
+        # Make a GET request to the GitHub API
+        response = requests.get("https://api.github.com/user", auth=auth)
+
+        # Check the status code
+        if response.status_code == 200:
+            # Display the user ID
+            user_id = response.json()["id"]
+            print(f"Your GitHub user ID is: {user_id}")
+        else:
+            print(f"Error: {response.status_code}")
+
+    except requests.exceptions.RequestException as e:
+        # Handle request exceptions
+        print(f"Error: {e}")
