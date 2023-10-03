93f9b44c79035e3205fef218ff9489cf74f75bb6
Anne-15 <wariiyuanne@gmail.com>
2023-10-03 11:26:02 +0300

    arguments

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,13 +1,12 @@
-if __name__ == '__main__':
+import sys
 
-    def print_arguments(*args):
-        def check(a):
-            print(len(args))
-            if(len(args) == 0):
-                print(".")
-            else:
-                index = 1
-                for n in args:
-                    print(index, ":", n)
-                    index += 1
-        return check(args)
+num_arguments = len(sys.argv) - 1
+print(f"{num_arguments} argument{'s' if num_arguments != 1 else ''}", end='')
+
+if num_arguments > 0:
+    print(":", end='')
+    for i, arg in enumerate(sys.argv[1:], start=1):
+        print(f"\n{i}: {arg}", end='')
+
+# Add a new line
+print()
