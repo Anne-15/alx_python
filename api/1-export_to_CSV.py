ec6f4cd3b6b3b90fade12d15b7e095a46ac80fe6
Anne-15 <wariiyuanne@gmail.com>
2023-10-04 22:12:17 +0300

    using username

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -15,7 +15,7 @@ def main():
     user_data = user_response.json()
 
     #  todos data
-    todos_url = f'https://jsonplaceholder.typicode.com/todos?userId={user_id}'
+    todos_url = f'https://jsonplaceholder.typicode.com/users/{user_id}/todos'
     todos_response = requests.get(todos_url)
     if todos_response.status_code != 200:
         print(f"Failed to fetch todos for user {user_data['name']}.")
@@ -28,7 +28,7 @@ def main():
         csv_writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
         for task in todos_data:
             csv_writer.writerow(
-                [user_id, user_data['name'], task['completed'], task['title']])
+                [user_id, user_data['username'], task['completed'], task['title']])
 
     print(f"Data exported to {csv_file}.")
 
