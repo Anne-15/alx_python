3c05215ff932cbf27bcd4abcbb5099799acb3447
Anne-15 <wariiyuanne@gmail.com>
2023-10-04 20:14:07 +0300

    making changes

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,28 +1,37 @@
-#!/usr/bin/python3
-"""
-Exporitng the data from the api requests to csv file
-"""
 import csv
 import requests
 import sys
 
-if __name__=="__main__":
+
+def main():
     user_id = sys.argv[1]
 
-    # getting user
-    user_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
-    user = requests.get(user_url)
-    user_data = user.json()
+    # user data
+    user_url = f'https://jsonplaceholder.typicode.com/users/{user_id}'
+    user_response = requests.get(user_url)
+    if user_response.status_code != 200:
+        print(f"User with ID {user_id} not found.")
+        return
+    user_data = user_response.json()
 
-    # getting todos
-    todo_url = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(user_id)
-    todos = requests.get(todo_url)
-    todos_data = todos.json()
+    #  todos data
+    todos_url = f'https://jsonplaceholder.typicode.com/todos?userId={user_id}'
+    todos_response = requests.get(todos_url)
+    if todos_response.status_code != 200:
+        print(f"Failed to fetch todos for user {user_data['name']}.")
+        return
+    todos_data = todos_response.json()
 
-    # exporting the data to csv
-    csv_file = "{}.csv".format(user_id)
-
-    with open(csv_file, 'w') as csvfile:
+    # CSV file
+    csv_file = f"{user_id}.csv"
+    with open(csv_file, 'w', newline='') as csvfile:
         csv_writer = csv.writer(csvfile)
         for task in todos_data:
-            csv_writer.writerow([user_id, str(user_data['name']), task['completed'],task['title']])
+            csv_writer.writerow(
+                [user_id, user_data['name'], task['completed'], task['title']])
+
+    print(f"Data exported to {csv_file}.")
+
+
+if __name__ == "__main__":
+    main()
