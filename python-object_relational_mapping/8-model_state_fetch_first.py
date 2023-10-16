09137d93225531db326acd27ca14e4cbfc311076
Anne-15 <wariiyuanne@gmail.com>
2023-10-16 21:35:21 +0300

    changes

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,28 +1,27 @@
-#!/usr/bin/python3
-"""
-Module containing the State class definition and Base instance
-"""
-
+import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
-import sys
 
-if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
 
-    engine = create_engine("mysql+mysqldb://{} \
-                           :{}@localhost:3306/{}"
-                           .format(username, password, database))
-    Base.metadata.create_all(engine)
+def print_first_state(username, password, db_name):
+    engine = create_engine(
+        f'mysql+mysqldb://{username}:{password}@localhost:3386/{db_name}')
+
     Session = sessionmaker(bind=engine)
     session = Session()
-    session.connection()
-    states = session.query(State).order_by(State.id).first()
-    if states is None:
-        print("Nothing")
+
+    first_state = session.query(State).order_by(State.id).first()
+
+    if first_state:
+        print(f"{first_state.id}: {first_state.name}")
     else:
-        print("{}: {}".format(states.id, states.name))
+        print("Nothing")
+
     session.close()
+
+
+if __name__ == "__main__":
+    username, password, db_name = sys.argv[1], sys.argv[2], sys.argv[3]
+    print_first_state(username, password, db_name)
+    
