f26322b850a464b2ef50f029dbd125e34c8573a3
Anne-15 <wariiyuanne@gmail.com>
2023-10-16 08:25:48 +0300

    docs

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -5,12 +5,6 @@ Attributes:
     __size (int): The size of the square (private).
 """
 BaseGeometry =  __import__('5-base_geometry.py').BaseGeometry
-"""
-Represents a square with a given size.
-
-Attributes:
-    __size (int): The size of the square (private).
-"""
 
 class Rectangle(BaseGeometry):
     """
